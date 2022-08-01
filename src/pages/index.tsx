dea49afcbb3c05403c6c0ab60299487b772efe40
Lukita <lucmaieski@gmail.com>
2022-08-01 19:12:11 -0300

    Create initial settings

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginForm from '../components/Login/LoginForm';
+import LoginFooter from '../components/Login/LoginFooter';
+
+export default function Home() {
+  return (
+    <div>
+      <LoginForm />
+      <LoginFooter />
+    </div>
+  );
+}
