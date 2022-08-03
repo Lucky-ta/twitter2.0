b9b163f520123909bfd0ca3fdaf44c3455ef925d
Lukita <lucmaieski@gmail.com>
2022-08-03 19:03:22 -0300

    Create main content component

--- src/components/Home/MainContent.tsx
+++ src/components/Home/MainContent.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
 function MainContent() {
-  return <div>Main Content</div>;
+  return (
+    <main>
+      <div>
+        <img src="" alt="main_profile_picture" />
+      </div>
+      <div>
+        <h2>User Name</h2>
+        <button type="button" aria-label="dots-option-button">
+          ...
+        </button>
+      </div>
+      <div>
+        <textarea name="" id="" cols={30} rows={10} />
+      </div>
+      <div>
+        <button type="button" aria-label="comment-button">
+          Comment Button
+        </button>
+        <button type="button" aria-label="retweet-button">
+          Retweet Button
+        </button>
+        <button type="button" aria-label="like-button">
+          Like Button
+        </button>
+        <button type="button" aria-label="share-button">
+          Share Button
+        </button>
+      </div>
+    </main>
+  );
 }
 
 export default MainContent;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../components/Home/Footer';
 import Header from '../components/Home/Header';
+import MainContent from '../components/Home/MainContent';
 
 function Home() {
   return (
     <div>
       <Header title="Página Inicial" />
+      <MainContent />
       <Footer />
     </div>
   );
