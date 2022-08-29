c98ae83c6082cabe47c8dc3d69efac600e94bf66
Lukita <lucmaieski@gmail.com>
2022-08-29 17:51:06 -0300

    Remove console.log

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,8 +1,6 @@
 import jwt from 'jsonwebtoken';
 
 const isAuthenticated = (token: string) => {
-  console.log(process.env.SECRET);
-
   const validate = jwt.verify(token, process.env.SECRET);
   return validate;
 };
