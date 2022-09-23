66b3b12e248a5e2daf1f9ffae7efbf41fbc6628f
Lukita <lucmaieski@gmail.com>
2022-09-23 15:44:12 -0300

    Create edit profile form

--- src/pages/userProfile/edit.tsx
+++ src/pages/userProfile/edit.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { HeaderSpacing } from '../../components/Home';
+import Header from '../../components/Home/Header';
+import ProfileInfo from '../../components/Profile/ProfileInfo';
+import { GlobalPageContainer } from '../../styles/globalContainer';
 
 function EditProfile() {
   return (
-    <div>
-      <h1>Edit profile</h1>
-    </div>
+    <GlobalPageContainer>
+      <Header title="Editar perfil" />
+      <HeaderSpacing />
+      <ProfileInfo />
+      <form>
+        <input type="text" placeholder="Nome" />
+        <button type="button">Salvar</button>
+      </form>
+      <button type="button">Excluir conta</button>
+    </GlobalPageContainer>
   );
 }
 
